a5a44907d7953838b504e407999d1c8e8206878e
Evelas <evelas94@gmail.com>
2021-05-08 18:27:19 +0300

    persisted

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import App from './App';
 import './scss/app.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/redux-store';
+// import store from './redux/redux-store';
 
-//import createAppStore from './redux/redux-store';
-// import { PersistGate } from 'redux-persist/integration/react';
-// const { persistor, store } = createAppStore();
+import createAppStore from './redux/redux-store';
+import { PersistGate } from 'redux-persist/integration/react';
+const { persistor, store } = createAppStore();
 
 ReactDOM.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router basename={process.env.PUBLIC_URL + `/stroisektor-shop/`}>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
       <App />
     </Provider>
   </Router>,
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,36 +4,36 @@ import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import rootSaga from '../redux/saga/sagas';
 
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// };
-// const persistedReducer = persistReducer(persistConfig, reducers);
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const saga = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware, saga)));
+//const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware, saga)));
 
 // let store = createStore(reducers, applyMiddleware(thunkMiddleware));
-saga.run(rootSaga);
-window.store = store;
+// saga.run(rootSaga);
+// window.store = store;
 
-export default store;
+// export default store;
 
-// const configureStore = () => {
-//   const store = createStore(
-//     persistedReducer,
-//     composeEnhancers(applyMiddleware(thunkMiddleware, saga)),
-//   );
-//   window.store = store;
-//   saga.run(rootSaga);
-//   const persistor = persistStore(store);
-//   return { store, persistor };
-// };
+const configureStore = () => {
+  const store = createStore(
+    persistedReducer,
+    composeEnhancers(applyMiddleware(thunkMiddleware, saga)),
+  );
+  window.store = store;
+  saga.run(rootSaga);
+  const persistor = persistStore(store);
+  return { store, persistor };
+};
 
-// // const { store } = configureStore();
+// const { store } = configureStore();
 
-// export default configureStore;
+export default configureStore;
