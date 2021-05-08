4dc7fc63719b25e8f1ef06cf1a04d539eb4c273f
Evelas <evelas94@gmail.com>
2021-05-08 17:09:23 +0300

    Updates

---
+++ precache-manifest.78b9dfb7aee3112a7b84928ef93df793.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d32a018be4c58d6e9a3d809a0b89b6",
+    "url": "/stroisektor/index.html"
+  },
+  {
+    "revision": "93d383588ecccb3b6ddc",
+    "url": "/stroisektor/static/css/2.15d247ec.chunk.css"
+  },
+  {
+    "revision": "b5f19f40b95906568c9d",
+    "url": "/stroisektor/static/css/main.06f02b28.chunk.css"
+  },
+  {
+    "revision": "93d383588ecccb3b6ddc",
+    "url": "/stroisektor/static/js/2.be011096.chunk.js"
+  },
+  {
+    "revision": "62f1b15ef45fdb2d1434bf701f16688b",
+    "url": "/stroisektor/static/js/2.be011096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f19f40b95906568c9d",
+    "url": "/stroisektor/static/js/main.c3fe67b9.chunk.js"
+  },
+  {
+    "revision": "79bdfb19fab50c97c880",
+    "url": "/stroisektor/static/js/runtime-main.6ae9e99b.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/stroisektor/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/stroisektor/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/stroisektor/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/stroisektor/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/stroisektor/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/stroisektor/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/stroisektor/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/stroisektor/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/stroisektor/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/stroisektor/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/stroisektor/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/stroisektor/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "ceb2faf089a96bede51ccde3693d012b",
+    "url": "/stroisektor/static/media/empty-cart.ceb2faf0.png"
+  }
+]);
